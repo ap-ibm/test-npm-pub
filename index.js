fc67ab255217bd08423774f6abc81b0f446d764a
Avinash Patel <avinash.patel2@ibm.com>
2020-01-15 10:29:21 +0000

    inital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const express = require("express");
+
+let app = express()
+
+app.get("/", (req, res) => {
+    res.send("hello-world");
+});
+
+app.listen(3000, function() {
+    console.log("listening");
+})
