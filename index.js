4eee92279bd0ac02790f76f5fca46dd14cbc1373
Avinash Patel <avinash.patel2@ibm.com>
2020-01-15 11:36:24 +0000

    updated version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 let app = express()
 
 app.get("/", (req, res) => {
-    res.send("hello-world");
+    res.send("goodbye cruel world");
 });
 
 app.listen(3000, function() {
